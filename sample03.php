7a24841658eaa827aa7be94302bcaec9b5538639
kakefuda <kaketomo3@gmail.com>
2015-04-22 05:38:56 +0000

    add sample03

---
+++ sample03.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>sample03</title>
+</head>
+<body>
+    <?php print('現在は、' . date('G時i分s秒') . 'です');?>
+</body>
+</html>
